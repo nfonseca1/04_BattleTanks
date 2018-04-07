36863fbe4b67e8738c01b8a96aaf2da422cbaad7
DESKTOP-NNE8748\Nathan <airide101@gmail.com>
2018-04-07 19:51:15 -0400

    Added BeginPlay() to PlayerController and logged possessed tank

--- BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
+++ BattleTanks/Source/BattleTanks/Private/TankPlayerController.cpp
@@ -3,6 +3,20 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessed tank: %s"), *ControlledTank->GetName());
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Tank Possessed"));
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
