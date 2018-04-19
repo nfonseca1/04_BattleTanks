fae13190b043995af546c49069b0c2039b3484ae
DESKTOP-NNE8748\Nathan <airide101@gmail.com>
2018-04-19 14:05:14 -0400

    Upgraded component architecture

--- BattleTanks/Source/BattleTanks/Public/TankMovementComponent.h
+++ BattleTanks/Source/BattleTanks/Public/TankMovementComponent.h
@@ -6,16 +6,25 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
  * 
  */
-UCLASS()
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANKS_API UTankMovementComponent : public UNavMovementComponent
 {
 	GENERATED_BODY()
 	
 public:
+	UFUNCTION(BlueprintCallable, Category = Setup)
+	void Initialize(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet);
+
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw);
+
+private:
+	UTankTrack* LeftTrack = nullptr;
+	UTankTrack* RightTrack = nullptr;
 	
 };
